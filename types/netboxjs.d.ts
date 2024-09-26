04fda0cf47209174299c791933d373d3038331f0
Kai Mosebach <kai@freshx.de>
2024-09-26 15:05:07 +0200

    add initial TS types

---
+++ types/netboxjs.d.ts
@@ -0,0 +1,21 @@
+// netboxjs.d.ts
+declare module 'netboxjs' {
+  import { AxiosRequestConfig, AxiosResponse } from 'axios';
+
+  interface NetboxOptions {
+    baseUrl: string;
+    apiKey: string;
+  }
+
+  class Netbox {
+    constructor(options: NetboxOptions);
+
+    get(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse>;
+    post(url: string, data: any, config?: AxiosRequestConfig): Promise<AxiosResponse>;
+    put(url: string, data: any, config?: AxiosRequestConfig): Promise<AxiosResponse>;
+    delete(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse>;
+  }
+
+  export = Netbox;
+}
+
